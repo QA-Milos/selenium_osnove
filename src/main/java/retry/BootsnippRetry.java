df23a7bf2077b232d402dc7d7c54d4bbb92ee8ca
Milos Josifovic <josifovicmilos77@gmail.com>
2023-10-12 15:01:50 +0200

    Retry Analyzer for bootsnipp.com tests

---
+++ src/main/java/retry/BootsnippRetry.java
@@ -0,0 +1,22 @@
+package retry;
+
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+
+public class BootsnippRetry implements IRetryAnalyzer {
+
+    private int count = 0;
+    private int maxRetry = 2;
+
+    @Override
+    public boolean retry(ITestResult iTestResult) {
+        if (count < maxRetry) {
+            count++;
+            return true;
+        }
+
+        return false;
+
+    }
+}
+
